90047af7aec8b06e49481f77eb1eb752101a24ec
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-01-30 00:21:55 +0700

    react practice

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
-const Header = ({ text }) => {
+import PropTypes from 'prop-types';
+
+const Header = ({ text = 'Feedback UI' }) => {
   return (
-    <header>
+    <header style={{ backgroundColor: 'black', color: '#ff6a95' }}>
       <div className="container">
         <h2>{text}</h2>
       </div>
     </header>
   );
 };
+
+Header.propTypes = {
+  text: PropTypes.string,
+};
+
 export default Header;
