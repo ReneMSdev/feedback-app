ea9b164313517ccdf4c85eca1c2340180f29224f
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-01-31 02:01:12 +0700

    added about icon link to about page and link back to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackData from './data/FeedbackData'
 import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,17 +24,34 @@ const App = () => {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList
-          feedback={feedback}
-          handleDelete={deleteFeedback}
-        />
+        <Routes>
+          {/* Main Page */}
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <AboutIconLink />
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          />
+          {/* About Page */}
+          <Route
+            path="/about"
+            element={<AboutPage />}
+          />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
